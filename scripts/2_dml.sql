82bff3fc2498a5ca402cde0e0d3b6aa8e9f9cf34
pedrazadumer <pedrazadumer@gmail.com>
2020-11-30 12:14:47 -0500

    Agregando el tipo Comprador

--- scripts/2_dml.sql
+++ scripts/2_dml.sql
@@ -2,6 +2,7 @@ USE `axpc`;
 
 -- Parece ser para los tipos de usuario (entre Productor y Comprador) (?)
 insert into usuarioTipo (usTiId, usTiNombre) values (1, 'PRODUCTOR');
+insert into usuarioTipo (usTiId, usTiNombre) values (2, 'COMPRADOR');
 
 -- Tipos de identificacion
 insert into usuarioTipoIdentificacion (usTiIdAbrev, ustiIdNombre) values ('C', 'Cedula de Ciudadania');
